8e9974870ce16524e7fd17ecb46bc3acd1575a43
Oriol Egea <oriol.egea@adevinta.com>
2024-02-16 13:51:16 +0100

    small improvements

--- app/page.js
+++ app/page.js
@@ -1,17 +1,19 @@
 "use client"
 import {MeetingForm} from "@/components/meeting-form";
 import {MeetingRecording} from "@/components/meeting-recording";
-import {useState} from 'react'
-import { getLocalStorage, getWindow } from "@/lib/utils";
+import {useEffect, useState} from 'react'
+import { getLocalStorage } from "@/lib/utils";
 
 export default function Home() {
 
   const [meeting, setMeeting] = useState(null);
 
-  if (getLocalStorage().getItem("openai-key") === null || getLocalStorage().getItem("openai-key") === ""){
-    getWindow().href = "/configuration";
-    return null
-  }
+  useEffect(() => {
+    if (getLocalStorage().getItem("openai-key") === null || getLocalStorage().getItem("openai-key") === ""){
+      window.href = "/configuration";
+      return null
+    }
+  }, []);
 
   const onNewMeeting = (meeting) => {
     setMeeting(meeting);
